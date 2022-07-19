2c7c5ae2b0ceef01439bf93ae3532866811b8d6c
nickhildebrant <nickhildebrant09@gmail.com>
2022-07-18 21:22:14 -0400

    Added Window Displaying

--- TutorialDX11/Window.cpp
+++ TutorialDX11/Window.cpp
@@ -1,6 +1,24 @@
+#include <Windows.h>
 #include "Window.h"
+
+LRESULT CALLBACK WinProc(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	return DefWindowProc(handle, msg, wparam, lparam);
+}
 
 Window::Window(int width, int height)
 {
+	// Define window style
+	WNDCLASS wc = { 0 };
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WinProc;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpszClassName = "DX11Tutorial";
+	RegisterClass(&wc);
 
+	// Create the window
+	CreateWindow("DX11Tutorial", "DX11 Tutorial",
+		WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_VISIBLE,
+		100, 100, width, height,
+		nullptr, nullptr, nullptr, nullptr);
 }
--- TutorialDX11/main.cpp
+++ TutorialDX11/main.cpp
@@ -3,8 +3,17 @@
 
 int CALLBACK WinMain(HINSTANCE appInstance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdCount)
 {
-	Window window(800, 800);
+	Window window(640, 640);
 
+	MSG msg = { 0 };
+	while (true)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
 	return 0;
 }
