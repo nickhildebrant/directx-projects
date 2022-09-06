a6694bd3a8d752ccd3bb701dd64a4f3b3fa54cae
nickhildebrant <nickhildebrant09@gmail.com>
2022-09-06 09:58:06 -0700

    Testing Git in Command Prompt

--- TutorialDX11/Triangle.cpp
+++ TutorialDX11/Triangle.cpp
@@ -87,5 +87,6 @@ void Triangle::createShaders(Renderer& renderer)
 		{ "POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
+
 	renderer.getDevice()->CreateInputLayout(layout, 2, vsData.data(), vsData.size(), &m_inputLayout);
 }
