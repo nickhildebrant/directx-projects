a593ead13442d8cadfb087dc286063a2a55bfa25
nickhildebrant <nickhildebrant09@gmail.com>
2022-09-23 16:59:20 -0700

    Keyboard Class for Input

---
+++ DXD11Rendering/Keyboard.cpp
@@ -0,0 +1,74 @@
+#include "Keyboard.h"
+
+bool Keyboard::isKeyPressed(unsigned char keycode) { return keyStates[keycode]; }
+
+bool Keyboard::isKeyEmpty() { return keyBuffer.empty(); }
+
+bool Keyboard::isCharEmpty() { return charBuffer.empty(); }
+
+// Clear queues
+void Keyboard::ClearKeyQueue() { keyBuffer = std::queue<Event>(); }
+void Keyboard::ClearCharQueue() { charBuffer = std::queue<char>(); }
+void Keyboard::ClearQueues()
+{
+	Keyboard::ClearKeyQueue();
+	Keyboard::ClearCharQueue();
+}
+
+// Auto repeat methods
+void Keyboard::EnableAutorepeat() { isAutorepeatEnabled = true; }
+void Keyboard::DisableAutorepeat() { isAutorepeatEnabled = false; }
+bool Keyboard::GetAutorepeatStatus() { return isAutorepeatEnabled; }
+
+Keyboard::Event Keyboard::ReadKey()
+{
+	if (keyBuffer.size() > 0u)
+	{
+		Keyboard::Event e = keyBuffer.front();
+		keyBuffer.pop(); // remove key from queue
+		return e;
+	}
+	else return Keyboard::Event();
+}
+
+char Keyboard::ReadChar()
+{
+	if (charBuffer.size() > 0u)
+	{
+		unsigned char charcode = charBuffer.front();
+		charBuffer.pop(); // remove char from queue
+		return charcode;
+	}
+	else return 0;
+}
+
+void Keyboard::OnKeyPressed(unsigned char keycode)
+{
+	keyStates[keycode] = true;
+	keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
+	TrimBuffer(keyBuffer);
+}
+
+void Keyboard::OnKeyReleased(unsigned char keycode)
+{
+	keyStates[keycode] = false;
+	keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
+	TrimBuffer(keyBuffer);
+}
+
+void Keyboard::OnChar(char character)
+{
+	charBuffer.push(character);
+	TrimBuffer(charBuffer);
+}
+
+void Keyboard::ClearState() { keyStates.reset(); }
+
+template<typename T>
+void Keyboard::TrimBuffer(std::queue<T>& buffer)
+{
+	while (buffer.size() > bufferSize)
+	{
+		buffer.pop();
+	}
+}
---
+++ DXD11Rendering/Keyboard.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <queue>
+#include <bitset>
+
+class Keyboard {
+	friend class Window;
+
+public:
+	class Event {
+	public:
+		enum class Type { Press, Release, Invalid };
+
+	private:
+		Type type;
+		unsigned char code;
+
+	public:
+		Event() : type(Type::Invalid), code(0u) {}
+		Event(Type type, unsigned char code) : type(type), code(code) {}
+
+		bool IsPressed() { return type == Type::Press; }
+		bool IsReleased() { return type == Type::Release; }
+		bool IsValid() { return type != Type::Invalid; }
+
+		unsigned char GetCode() { return code; }
+	};
+
+	Keyboard() = default;
+	Keyboard(const Keyboard&) = delete;
+	Keyboard& operator=(const Keyboard&) = delete;
+
+	// *** Event methods
+	// key events
+	bool isKeyPressed(unsigned char keycode);
+	bool isKeyEmpty();
+	Event ReadKey();
+	void ClearKeyQueue();
+
+	// char events
+	bool isCharEmpty();
+	char ReadChar();
+	void ClearCharQueue();
+
+	void ClearQueues();
+	// ******
+
+	// Autorepeat
+	void EnableAutorepeat();
+	void DisableAutorepeat();
+	bool GetAutorepeatStatus();
+
+private:
+	void OnKeyPressed(unsigned char keycode);
+	void OnKeyReleased(unsigned char keycode);
+	void OnChar(char character);
+	void ClearState();	// Clears the bitset with key states
+
+	template<typename T>
+	static void TrimBuffer(std::queue<T>& buffer);	// Trims queue back to 16
+
+	static constexpr unsigned int nKeys = 256u;
+	static constexpr unsigned int bufferSize = 16u;
+	bool isAutorepeatEnabled = false;
+	std::bitset<nKeys> keyStates;
+	std::queue<Event> keyBuffer;
+	std::queue<char> charBuffer;
+};
