af49d155b09fd4daf58c733097de733aa05cf310
nickhildebrant <nickhildebrant09@gmail.com>
2023-06-23 13:57:28 -0700

    Implemented Rotation Matrix for Spinning Shape

--- DXD11Rendering/Application.cpp
+++ DXD11Rendering/Application.cpp
@@ -23,6 +23,6 @@ void Application::DoFrame()
 {
 	const float c = sin(m_timer.PeekDelta()) / 2.0f + 0.5f; // changes color on background, based on sin wave
 	m_window.getRenderer().ClearBuffer(c, c, 1.0f);			// sets background color
-	m_window.getRenderer().DrawTestTriangle(1.0f);			// Draws triangle
+	m_window.getRenderer().DrawTestTriangle(m_timer.PeekDelta());			// Draws triangle
 	m_window.getRenderer().EndFrame();
 }
--- DXD11Rendering/VertexShader.hlsl
+++ DXD11Rendering/VertexShader.hlsl
@@ -3,10 +3,14 @@ struct VertexShaderOutput {
 	float4 position : SV_Position;
 };
 
+cbuffer ConstantBuffer {
+	matrix transform;
+};
+
 VertexShaderOutput main(float2 pos : Position, float3 color : Color)
 {
 	VertexShaderOutput output;
-	output.position = float4(pos.x, pos.y, 0.0f, 1.0f);
+	output.position = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
 	output.color = color;
 
 	return output;
