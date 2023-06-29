55fec2c3fdf0e0e18fac2879f3951b67cbed76c3
nickhildebrant <nickhildebrant09@gmail.com>
2023-06-29 10:54:27 -0700

    Adding Primitives

--- DXD11Rendering/Application.cpp
+++ DXD11Rendering/Application.cpp
@@ -10,10 +10,10 @@ Application::Application() : m_window(640, 480, "3D Renderer")
 	// Random cubes
 	std::mt19937 rng(std::random_device{}());
 	std::uniform_real_distribution<float> adist(0.0f, 3.1415f * 2.0f);
-	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 2.0f);
-	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.3f);
+	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 1.0f);
+	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.08f);
 	std::uniform_real_distribution<float> rdist(6.0f, 20.0f);
-	for (auto i = 0; i < 80; i++)
+	for (auto i = 0; i < 180; i++)
 	{
 		m_boxes.push_back(std::make_unique<Box>(m_window.getRenderer(), rng, adist, ddist, odist, rdist));
 	}
---
+++ DXD11Rendering/IndexedTriangleList.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <DirectXMath.h>
+
+template<class T>
+class IndexedTriangleList {
+public:
+	IndexedTriangleList() = default;
+	IndexedTriangleList(std::vector<T> vertices_in, std::vector<unsigned short> indices_in)
+		: this.vertices(std::move(vertices_in)), this.indices(std::move(indices_in))
+	{
+		assert(vertices.size() > 2);
+		assert(indices.size() % 3 == 0);
+	}
+
+	void Transform(DirectX::FXMMATRIX matrix)
+	{
+		for (auto& vertex : vertices)
+		{
+			const DirectX::XMVector pos = DirectX::XMLoadFloat3(&vertex.position);
+			DirectX::XMStoreFloat3(&vertex.position, DirectX::XMVector3Transform(position, matrix));
+		}
+	}
+
+public:
+	std::vector<T> vertices;
+	std::vector<unsigned short> indices;
+};
