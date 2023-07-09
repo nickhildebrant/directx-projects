4a9e551fda44758a5257638df992161f49e4feb7
nickhildebrant <nickhildebrant09@gmail.com>
2023-07-09 10:52:50 -0700

    Handling Mouse and Keyboard Messages
    TODO: Fix Read Access Violation

--- GameEngineDX11/Window.h
+++ GameEngineDX11/Window.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <Windows.h>
 
+#include "Keyboard.h"
+#include "Mouse.h"
+
 class Window {
 public:
 	Window( int width, int height, const char* title );
@@ -10,10 +13,14 @@ public:
 	int getHeight();
 	HWND getHandle();
 
-	//Keyboard keyboard;
-	//Mouse mouse;
+	Keyboard keyboard;
+	Mouse mouse;
 
 private:
 	int m_width, m_height;
 	HWND m_handle;
+
+	static LRESULT WINAPI HandleMsgSetup( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
+	static LRESULT WINAPI HandleMsgThunk( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
+	LRESULT HandleMsg( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
 };
