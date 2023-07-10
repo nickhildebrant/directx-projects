40d8dfb4f535f8b741c4661b57d3e859e8c33d6b
nickhildebrant <nickhildebrant09@gmail.com>
2023-07-10 15:23:58 -0700

    Revert "Handling Mouse and Keyboard Messages"
    This reverts commit 4a9e551fda44758a5257638df992161f49e4feb7.

--- GameEngineDX11/Window.h
+++ GameEngineDX11/Window.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <Windows.h>
 
-#include "Keyboard.h"
-#include "Mouse.h"
-
 class Window {
 public:
 	Window( int width, int height, const char* title );
@@ -13,14 +10,10 @@ public:
 	int getHeight();
 	HWND getHandle();
 
-	Keyboard keyboard;
-	Mouse mouse;
+	//Keyboard keyboard;
+	//Mouse mouse;
 
 private:
 	int m_width, m_height;
 	HWND m_handle;
-
-	static LRESULT WINAPI HandleMsgSetup( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
-	static LRESULT WINAPI HandleMsgThunk( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
-	LRESULT HandleMsg( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
 };
