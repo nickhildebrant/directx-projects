c9e99b0a3b53fd6af3f6a71b2e9fd5eb6f7ae030
nickhildebrant <nickhildebrant09@gmail.com>
2023-07-13 18:26:03 -0700

    Updated PixelShader.h

--- GameEngineDX11/PixelShader.h
+++ GameEngineDX11/PixelShader.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Bindable.h"
+
+class PixelShader : public Bindable {
+public:
+	PixelShader( Renderer& renderer, const std::wstring& path );
+	void Bind( Renderer& renderer );
+
+protected:
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> m_pixelShader;
+};
