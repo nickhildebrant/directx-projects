a57df050ace65d7746a31f10c9de2673d260996c
nickhildebrant <nickhildebrant09@gmail.com>
2023-07-14 17:06:15 -0700

    Whatever, TODO: VS_4_0 PrimitiveID

--- GameEngineDX11/WinMain.cpp
+++ GameEngineDX11/WinMain.cpp
@@ -12,7 +12,15 @@ int WINAPI WinMain( HINSTANCE appInstance, HINSTANCE prevInstance, LPSTR cmdLine
 	Window window( 640, 480, "My Game Engine" );
 	Renderer renderer( window );
 	//Triangle triangle( renderer );
-	Box box( renderer );
+
+    // Random cube
+	std::mt19937 rng( std::random_device{}( ) );
+	std::uniform_real_distribution<float> adist( 0.0f, 3.1415f * 2.0f );
+	std::uniform_real_distribution<float> ddist( 0.0f, 3.1415f * 2.0f );
+	std::uniform_real_distribution<float> odist( 0.0f, 3.1415f * 0.3f );
+	std::uniform_real_distribution<float> rdist( 6.0f, 20.0f );
+	std::uniform_real_distribution<float> bdist( 0.0f, 1.0f );
+	Box box( renderer, rng, adist, ddist, odist, rdist, bdist );
 
 	MSG msg = { 0 };
 	while ( true )
@@ -32,7 +40,7 @@ int WINAPI WinMain( HINSTANCE appInstance, HINSTANCE prevInstance, LPSTR cmdLine
 
 		// Render polygons
 		//triangle.draw( renderer );
-
+		box.Draw( renderer );
 		renderer.EndFrame();
 	}
 
