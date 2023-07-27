65fed23836d901a6ff78893e0056f9175c376b4a
nickhildebrant <nickhildebrant09@gmail.com>
2023-07-27 14:14:49 -0700

    Changed 'V' Will need to change when Camera location can be put in

--- Tutorial2/PhongPixelShader.hlsl
+++ Tutorial2/PhongPixelShader.hlsl
@@ -19,8 +19,6 @@ cbuffer ObjectConstantBuffer
 
 static const float4 specularColor = { 1.0f, 1.0f, 1.0f, 1.0f };
 
-static const float4 cameraPosition = { 0.0f, 0.0f, 20.0f, 1.0f };
-
 static const float ambientIntensity = 1.0f;
 static const float lightIntensity = 1.0f;
 
@@ -30,7 +28,7 @@ float4 main( float4 worldPosition : Position, float4 normal : Normal) : SV_Targe
     float distance = length(lightPosition - worldPosition);
     float4 L = normalize(lightPosition - worldPosition);
     float4 N = normalize(normal);
-    float4 V = normalize(cameraPosition - worldPosition);
+    float4 V = normalize(worldPosition);
     float4 R = reflect(-L, N);
     
     float4 ambient = ambientColor * ambientIntensity;
